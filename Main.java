0e9fd7fc360aff33b1c9e29410cf57a8354862bd
Dinesh babu <110668113+dineshtdb@users.noreply.github.com>
2025-03-24 14:47:12 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static int add(int a, int b) {
-       
+       return a+b;
     }
     public static void main(String args[]){
-      
+        int a=10,b=20;
+     System.out.print( add(a,b));
     }
 }

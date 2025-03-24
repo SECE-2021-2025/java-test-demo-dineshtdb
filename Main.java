6de6071462365f530b654cb48a7e525592bae2df
Dinesh babu <110668113+dineshtdb@users.noreply.github.com>
2025-03-24 14:50:19 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
+import java.util.*;
 public class Main {
     public static int add(int a, int b) {
        return a+b;
     }
     public static void main(String args[]){
-        int a=10,b=20;
-     System.out.print( add(a,b));
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt(),b=sc.nextInt();
+        System.out.print( add(a,b));
     }
 }
